4df727441a0a7400a3e39e04eea11ea942202de3
Kingsley Luong <ducsuongluong@gmail.com>
2022-01-21 14:15:16 -0500

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const any = require("./4-name");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+// sayHi(any.john);
+// sayHi(any.peter);
+// console.log(data);
